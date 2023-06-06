daa769ff7a4b07d0851ab7f56129ec6a3f44e177
baobob-dev <timfx35@gmail.com>
2023-06-06 17:26:44 +0200

    update

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,7 +1,3 @@
-const wait = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const home = document.querySelector(".home")
     const competences = document.querySelector(".competences")
@@ -10,47 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const colorFont = document.querySelector(':root')
 
     const sectionHome = document.querySelector(".home-section")
+    const secctionAbout = document.querySelector(".about-section")
     const sectionComp = document.querySelector(".competence-section")
     const sectionInteret = document.querySelector(".interet-section")
     const sectionContact = document.querySelector(".contact-section")
 
     home.addEventListener("click", async () => {
-        await wait(100).then(() => {
-            sectionHome.style.display = "block"
-            sectionComp.style.display = "none"
-            sectionInteret.style.display = "none"
-            sectionContact.style.display = "none"
-            colorFont.style.setProperty('--color-font', '#fff')
-        })
     })
 
     competences.addEventListener("click", async () => {
-        await wait(100).then(() => {
-            sectionHome.style.display = "none"
-            sectionComp.style.display = "block"
-            sectionInteret.style.display = "none"
-            sectionContact.style.display = "none"
-            colorFont.style.setProperty('--color-font', '#1e1e1e')
-        })
     })
 
     interest.addEventListener("click", async () => {
-        await wait(100).then(() => {
-            sectionHome.style.display = "none"
-            sectionComp.style.display = "none"
-            sectionInteret.style.display = "block"
-            sectionContact.style.display = "none"
-            colorFont.style.setProperty('--color-font', '#1e1e1e')
-        })
     })
 
     contact.addEventListener("click", async () => {
-        await wait(100).then(() => {
-            sectionHome.style.display = "none"
-            sectionComp.style.display = "none"
-            sectionInteret.style.display = "none"
-            sectionContact.style.display = "block"
-            colorFont.style.setProperty('--color-font', '#1e1e1e')
-        })
     })
 })
