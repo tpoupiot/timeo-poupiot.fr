6b9ac18f7aa7131897a0ee3e20b89836ed6cf32e
baobob-dev <timfx35@gmail.com>
2023-06-10 13:56:08 +0200

    new port

---
+++ js_global/pages.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const home = document.querySelector(".home")
+    const competences = document.querySelector(".competences")
+    const interest = document.querySelector(".interest")
+    const contact = document.querySelector(".contact")
+    const colorFont = document.querySelector(':root')
+
+    const sectionHome = document.querySelector(".home-section")
+    const secctionAbout = document.querySelector(".about-section")
+    const sectionComp = document.querySelector(".competence-section")
+    const sectionInteret = document.querySelector(".interet-section")
+    const sectionContact = document.querySelector(".contact-section")
+
+    home.addEventListener("click", async () => {
+    })
+
+    competences.addEventListener("click", async () => {
+    })
+
+    interest.addEventListener("click", async () => {
+    })
+
+    contact.addEventListener("click", async () => {
+    })
+})
